df1b863c547ace163cab90e1974304a891a16f41
Robert Tate <roberttate@Roberts-MacBook-Pro.local>
2019-10-17 11:52:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+const findMatching = (drivers, name) => {
+  return drivers.filter(driverName => {
+    return driverName.toUpperCase() === name.toUpperCase();
+  });
+}
+
+const fuzzyMatch = (drivers, name) => {
+  return drivers.filter(driverName => {
+    return driverName[0] === name[0];
+  });
+}
+
+const matchName = (drivers, name) => {
+  return drivers.filter(driver => {
+    return driver.name === name;
+  });
+}

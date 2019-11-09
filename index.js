2d9299fe32b1e8b92be62a8f486ea268eba0a5c3
davisjustinw <davisjustinw@gmail.com>
2019-11-08 22:33:22 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here
+const findMatching = ( drivers, name ) =>
+  drivers.filter( x => x.toLowerCase() === name.toLowerCase() )
+
+const fuzzyMatch = ( drivers, str ) =>
+  drivers.filter( x => x.startsWith( str ))
+
+const matchName = ( drivers, str ) =>
+  drivers.filter( x => x.name === str )

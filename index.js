4593facade9e28c3ca742f1fb6983c0a3560b523
Kate F <follett.katie@gmail.com>
2019-11-20 19:27:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution here
+const findMatching = (drivers, name) => drivers.filter(dr => dr.toLowerCase() == name.toLowerCase());
+
+
+const fuzzyMatch = (drivers, str) => drivers.filter(dr => dr.slice(0, str.length) == str);
+
+const matchName = (drivers, str) => drivers.filter(({name}) => name == str);

6a818ea049545ba78f00d94b43ec1deb06d8c193
Mark Delgado <markpnoi26@gmail.com>
2019-12-11 19:40:23 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+function findMatching(driversArray, string) {
+  return driversArray.filter( driver => {
+    if (driver.toLowerCase() === string.toLowerCase()) {
+      return driver;
+    };
+  });
+};
+
+function fuzzyMatch(driversArray, string) {
+  return driversArray.filter(driver => {
+    let stringCount = string.length
+    if (driver.slice(0, stringCount) === string) {
+      return driver;
+    };
+  });
+};
+
+
+function matchName(driversArray, string) {
+  return driversArray.filter(driver => {
+    if (driver.name === string) {
+      return driver;
+    };
+  });
+};

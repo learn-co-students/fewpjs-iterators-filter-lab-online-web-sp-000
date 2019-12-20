fff56c5600b8a159dcdece6dcface06cd25f7021
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2019-12-20 15:02:56 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(arr, str) {
+    return arr.filter(function(driver) {
+        return driver.toLowerCase() === str.toLowerCase();
+    });
+}
+
+function fuzzyMatch(drivers, str) {
+    return drivers.filter(function(driver) {
+        return driver[0] === str[0];
+    });
+}
+
+function matchName(drivers, str) {
+    return drivers.filter(function(driver) {
+        return driver.name === str;
+    });
+}

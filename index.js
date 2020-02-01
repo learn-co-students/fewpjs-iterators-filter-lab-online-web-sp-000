c8e2621b58ee5fb64e6d6b817ee9d98db1f5fbdf
Bernadette Davis <bsd231@nyu.edu>
2020-02-01 01:17:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+
+
+function findMatching(drivers, searchValue){
+  return drivers.filter(driver => driver.toLowerCase() === searchValue.toLowerCase());
+}
+
+function fuzzyMatch(drivers, searchValue){
+  return drivers.filter(driver => driver.toLowerCase().substring(0,2) === searchValue.toLowerCase());
+}
+
+function matchName(drivers, searchValue){
+  return drivers.filter(driver => driver.name === searchValue);
+}

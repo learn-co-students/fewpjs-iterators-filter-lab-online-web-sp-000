0eda7c280e45083eca45d127abe785038d90a2c5
github username <github email address>
2020-03-05 11:45:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+function findMatching(array, name) {
+    return array.filter(person => {
+       return person.toLowerCase() == name.toLowerCase()
+    })  
+}
+
+function fuzzyMatch(array, name) {
+    return array.filter(person => {
+        let x = name.split("")
+        let y = person.split("")
+        if (x.length === 2) {
+       return  person[x.length -1] === x[x.length -1] && person[0] === x[0]
+        } else {
+           person[x.length -1] === x[x.length -1]
+        }
+    })
+}
+
+function matchName(array, name) {
+    return array.filter(person => {
+        return person.name === name
+    })
+}

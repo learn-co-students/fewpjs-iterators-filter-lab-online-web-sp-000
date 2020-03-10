070f92b182577e54b2a1d12c2cc84ff4bc3bd6d1
Freddy Silber <freddy.silber@cloudcoach.com>
2020-03-10 15:18:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+const findMatching = (array, name) => {
+  return array.filter(driverName => {
+    return driverName.toLowerCase() === name.toLowerCase()
+  })
+}
+
+const fuzzyMatch = (drivers, name) => {
+  let lengthOfName = name.length
+  return drivers.filter(driverName => {
+    return driverName.slice(0, lengthOfName) === name
+  })
+}
+
+const matchName = (drivers, name) => {
+  return drivers.filter(driver => {
+    return driver.name.toLowerCase() === name.toLowerCase()
+  })
+}

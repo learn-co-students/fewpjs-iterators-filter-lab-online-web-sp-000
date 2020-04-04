8d228e7fb7520cb49357fa30500a060d32ff738c
Malcolm <echelon1089@gmail.com>
2020-04-04 13:29:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+function findMatching(array, driver){
+    let filter = array.filter(d => d === driver || d.toLowerCase() === driver.toLowerCase())
+    return filter;
+}
+
+function fuzzyMatch(array, driver){
+    return array.filter(m => m[0] === driver.[0])
+
+}
+
+// function matchName( array, driver){
+//     return array.filter(m => m.name === driver)
+// }

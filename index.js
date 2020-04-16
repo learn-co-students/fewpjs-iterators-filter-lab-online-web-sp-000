01f9703e3bd83ecc40785e6c0f315fe247648aad
Soner Mezgitci <mezgitci9@gmail.com>
2020-04-16 13:28:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, string){
+    return drivers.filter(driver => {
+      (driver.chartArt(0).toLowerCase() === string.chartArt(0).toLowerCase())? driver: string 
+    })
+    // console.log(taxi)
+  return drivers
+}
+
+// function fuzzyMatch(drivers, string){
+    
+// }

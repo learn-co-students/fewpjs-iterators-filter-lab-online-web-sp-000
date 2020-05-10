0850da0e2aec5156f523ed6a4e2eef0e4050fa44
github username <github email address>
2020-05-10 17:06:13 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+function findMatching(arrOfDrivers, nameStr){
+  return arrOfDrivers.filter(d => {
+    return d.toUpperCase() === nameStr.toUpperCase();
+  });
+}
+
+function fuzzyMatch(arrOfDrivers, partialNameStr){
+  return arrOfDrivers.filter(d => {
+    let l = partialNameStr.length;
+     let namePart = d.slice(0, l);
+    return namePart.toUpperCase() === partialNameStr.toUpperCase();
+  })
+}
+
+function matchName(arrOfDriverObjs, nameStr) {
+  return arrOfDriverObjs.filter(dobj => {
+    return dobj.name === nameStr;
+  })
+}
+
+// let driverArr = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']; 
+// let str = 'sam';
+// fuzzyMatch(driverArr, str);
+

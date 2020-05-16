ba4d78ae6e30ba2790f6f424733563c30ad824a5
azbury <acasbury25@aol.com>
2020-05-16 00:39:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(array, name) {
+    return array.filter(e => e.toUpperCase() === name.toUpperCase())
+}
+
+function fuzzyMatch(array, name) {
+    return array.filter(e => e[0] + e[1] === name)
+}
+
+function matchName(array, name) {
+    return array.filter(e => e.name === name)
+}

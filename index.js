b87447d23426606f8ce7790de4fe57d4c78c485e
zdsmit <zdsmith001@gmail.com>
2020-05-17 09:09:28 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter(d => d.slice(1) === name.slice(1))
+}
+
+function fuzzyMatch(drivers, name) {
+  return drivers.filter(d => d.charAt(0) === name.charAt(0))
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(d => d.name === name)
+}

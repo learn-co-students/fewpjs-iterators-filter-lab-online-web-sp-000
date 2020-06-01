e301ff4026b05c73acf82ecfb679aae6ab7dd13a
jkellyphilly <joel.kelly.philly@gmail.com>
2020-06-01 13:19:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, input) {
+  return drivers.filter(driver => {
+    return (driver.toLowerCase() === input.toLowerCase());
+  });
+}
+
+function fuzzyMatch(drivers, input) {
+  return drivers.filter(driver => {
+    return (driver.slice(0, input.length) === input);
+  });
+}
+
+function matchName(drivers, input) {
+  return drivers.filter(driver => {
+    return (driver.name === input);
+  });
+}

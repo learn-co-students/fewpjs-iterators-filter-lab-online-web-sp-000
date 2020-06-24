13a3f1d860a33101e66932c2d56d6259f54a0372
nathan-stamand <nathansaint810@gmail.com>
2020-06-24 10:21:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, string) {
+  return drivers.filter(dr => dr.toUpperCase() === string.toUpperCase())
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(dr => dr[0].toUpperCase() === string[0].toUpperCase())
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(dr => dr.name === string)
+}

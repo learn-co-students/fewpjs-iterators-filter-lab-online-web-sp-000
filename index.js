6e94bae11516bc71cc67c02cb4fc2e75efa3dc47
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-08-08 00:54:44 +0200

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(arr, string) {
+    return arr.filter(d => d.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(arr, string) {
+    return arr.filter(d => d.slice(0, string.length) === string);
+}
+
+function matchName(arr, string) {
+    return arr.filter(d => d.name === string);
+}

bf2901bee67fbb0dfd0de22401333677bda6c626
Azraf Chowdhury <achowdhur@ltu.edu>
2020-08-19 05:03:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(array, string) {
+  return array.filter(x => x.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(array, string) {
+  return array.filter(x => x.slice(0, string.length) === string)
+}
+
+function matchName(array, string) {
+  return array.filter(x => x.name === string)
+}

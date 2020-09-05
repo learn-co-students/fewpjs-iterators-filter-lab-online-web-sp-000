cc9a58a5bd4848da43d44aff479aa1e5cdf50bb9
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-09-05 16:02:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+drivers = ['steve', 'ryan', 'dillan', 'dustin', 'jody', 'zach', 'brandon']
+
+function findMatching(drivers, name) { 
+  return drivers.filter(function (driverName) { 
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+} 
+
+function fuzzyMatch(drivers, partialName) {
+  let lengthOfName = partialName.length;
+  return drivers.filter(function (driverName) { 
+    return driverName.slice(0, lengthOfName) === partialName;
+  });
+} 
+
+function matchName(drivers, name) {
+  return drivers.filter(function (driver) { 
+    return driver.name.toLowerCase() === name.toLowerCase()
+  });
+}

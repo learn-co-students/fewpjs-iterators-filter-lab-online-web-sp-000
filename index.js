3443b2a19901070216f5d4faadbc3cab78cc8d7d
Scott Hoisington <hoyzington@gmail.com>
2020-09-26 23:34:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(drivers, name) {
+  const query = name[0].toLowerCase() + name.slice(1);
+  return drivers.filter((i) => { return i === name || i === query });
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter((i) => { return (i[0] === string[0]) && (i[1] === string[1]) });
+}
+
+function matchName(drivers, name) {
+  return drivers.filter((i) => { return i.name === name });
+}

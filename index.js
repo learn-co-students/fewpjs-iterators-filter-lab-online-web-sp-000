cd5aac3d7a40438ac92d6c24e241a398ea92e2f1
Chance Smith <chancesmith97@hotmail.com>
2020-09-27 20:53:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, str) {
+  return drivers.filter(n => n.toUpperCase() == str.toUpperCase());
+}
+
+function fuzzyMatch(drivers, str){
+  return drivers.filter(n => n.charAt(0) == str.charAt(0));
+}
+
+function matchName(drivers, str){
+  return drivers.filter(n => n.name == str);
+}

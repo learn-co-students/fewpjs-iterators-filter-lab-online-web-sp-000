bacb4c2857a45e67fb104dcd9dcce4d71d89e1ae
carrollm2 <carrollm2@gmail.com>
2020-10-16 22:19:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+
+
+function findMatching(drivers, driver){
+    let matchingDrivers = drivers.filter(d => {
+        return d.toLowerCase() === driver.toLowerCase();
+    });
+    return matchingDrivers;
+}
+
+function fuzzyMatch(drivers, driver){
+    let fuzzyDrivers = drivers.filter(d => {
+        return d.slice(0, 2).toLowerCase() === driver.slice(0, 2).toLowerCase();
+    });
+    return fuzzyDrivers;    
+
+}
+
+function matchName(drivers, driver){
+    let matchedName = drivers.filter(d => {
+        if (d.name.toLowerCase() === driver.toLowerCase()){
+            return d;
+        }
+    });
+    return matchedName;  
+}

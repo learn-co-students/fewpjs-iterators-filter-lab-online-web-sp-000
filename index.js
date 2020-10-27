98aa8b86a8db3cdb1fb46050434d556967c54386
rohanphillips <rohan@solutions-insurance.com>
2020-10-27 09:03:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+function findMatching(drivers, name){
+  return drivers.filter(function(d) {
+    return d.toLowerCase() === name.toLowerCase();
+  })
+}
+
+function fuzzyMatch(drivers, match){
+  return drivers.filter(function(d){
+    return d.substring(0, match.length).includes(match);
+  })
+}
+
+function matchName(drivers, match){
+  return drivers.filter(function(d){
+    return d.name === match;
+  })
+}

cd93451a46daaea42feefc81ee41c95931078fc3
blumenthaler <blumenthaleralex@gmail.com>
2020-11-17 16:07:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(array, string) {
+    return array.filter(function (driver) {
+      return driver.toLowerCase() === string.toLowerCase();
+    });
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(function (driver) {
+        return ((driver.charAt(0) + driver.charAt(1)) === (string.charAt(0) + string.charAt(1)))
+    })
+}
+
+function matchName(array, string) {
+    return array.filter(function (object) {
+        return object.name === string
+    })
+}
+
+

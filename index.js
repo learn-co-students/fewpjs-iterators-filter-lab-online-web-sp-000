512d5ee975fb6717c12416876d8850c1c14cad4f
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-11-22 17:14:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here
+
+//1.
+// Write findMatching- This function takes an array of drivers and a string as arguments,
+//and returns the matching list of drivers. The function should be case insensitive.
+findMatching = drivers => {
+  let result = drivers.filter(name => name.result === name)
+  return !!result ? result : []
+}

0c15c5ded650a321d5e67a209630489ec282cd17
Scott Hoisington <hoyzington@gmail.com>
2020-12-02 05:00:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 function findMatching(drivers, name) {
-  const query = name[0].toLowerCase() + name.slice(1);
-  return drivers.filter((i) => { return i === name || i === query });
+  return drivers.filter((driver) => {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
 }
 
 function fuzzyMatch(drivers, string) {
-  return drivers.filter((i) => { return (i[0] === string[0]) && (i[1] === string[1]) });
+  return drivers.filter((driver) => {
+    return driver.startsWith(string);
+  });
 }
 
 function matchName(drivers, name) {
-  return drivers.filter((i) => { return i.name === name });
+  return drivers.filter((driver) => { return driver.name === name });
 }

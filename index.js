be71616afd8d0eeac3a8d0125fc6bd325496c8ba
Joseph <DeGregorio.Joseph@gmail.com>
2020-12-21 10:30:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+function findMatching(array, value){
+    return array.filter(n =>{
+        if (n.toLowerCase() === value.toLowerCase()){
+            return n
+        }
+    });
+}
+
+function fuzzyMatch(array, value){
+    return array.filter(n =>{
+        if ((n.split("")[0]) === (value.split("")[0])){
+            if ((n.split("")[1]) === (value.split("")[1])){
+                return n;
+            };
+        };
+    });
+}
+
+function matchName(array, value){
+    return array.filter(n =>{
+        if (n.name === value){
+            return n;
+        };
+    });
+}

0cf94c29ca6f4f618ab8d37d63ceb90c30b6a117
ismayton <ian.mayton@gmail.com>
2020-12-30 11:21:47 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+function findMatching(array, name) {
+    return array.filter(arrayName => { 
+        return arrayName == name || arrayName == name.toLowerCase(); 
+    })
+    
+}
+
+function fuzzyMatch(array, name) {
+    return array.filter(arrayName => {
+        return arrayName.slice(0, name.length) === name
+    })
+}
+
+function matchName(array, name) {
+    return array.filter( arrayName => {
+        return arrayName.name === name
+    })
+}

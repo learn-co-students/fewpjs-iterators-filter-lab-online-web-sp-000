9c378b10a1852b2012873c5bd904aa3f01b4e9f1
Yumacorn <ReachRyanMui@gmail.com>
2021-01-07 00:54:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+const drivers2 = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+function findMatching(set, string) {
+    let x = set.filter(result => {
+        return string.toUpperCase() === result.toUpperCase();
+    });
+    return x;
+}
+
+function fuzzyMatch(set, string) {
+    let x = set.filter(result => {
+        return result.startsWith(string);
+    });
+    return x;
+}
+
+function matchName(set, string) {
+    let x = set.filter(result => {
+        return string === result.name;
+    });
+    return x;
+}

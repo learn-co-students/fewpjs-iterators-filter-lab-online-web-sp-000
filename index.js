3a382365ab1f8ffd2860c1ab0f224aab26b40d03
learn-co-git <argus.two.2@protonmail.com>
2021-01-25 21:33:17 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, string) {
+  return drivers.filter(ele => ele.toLowerCase() == string.toLowerCase());
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(ele => ele.toLowerCase().slice(0, string.length) == string.toLowerCase())
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(ele => ele.name === string);
+}

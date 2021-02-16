7eda26ec14dbdee15f9ec428e2f816a0a2e1a37c
khoinguyenkc <khoinguyenkc@gmail.com>
2021-02-15 16:50:09 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(array, queryString) {
+    return array.filter( function(element) { return element.toLowerCase() === queryString.toLowerCase() })
+}
+
+function fuzzyMatch(array, queryString) {
+    return array.filter( function(element) { 
+        return element.toLowerCase().slice(0, queryString.length) === queryString.toLowerCase() 
+    })
+
+}
+
+function matchName(arrayOfObjects, queryString) {
+    return arrayOfObjects.filter( function(element) { return element.name.toLowerCase() === queryString.toLowerCase() })
+
+}

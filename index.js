4ec417024a9840acc17be446dfe331a9990578cb
keelsk <keelsk@gmail.com>
2021-02-21 11:40:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+
+function findMatching(drivers, string){
+    let matchedDrivers = drivers.filter(n=>{
+        return n.toLowerCase() === string.toLowerCase()
+    })
+
+    return matchedDrivers
+}
+
+
+function fuzzyMatch (drivers, string) {
+    let strLength = string.length
+    let fuzzyDrivers = drivers.filter(n=>{
+        return n.substring(0,strLength) === string
+    })
+
+    return fuzzyDrivers
+}
+
+function matchName(drivers, string) {
+    let matchedNames = drivers.filter(n=>{
+        return n.name === string
+    })
+
+    return matchedNames
+}

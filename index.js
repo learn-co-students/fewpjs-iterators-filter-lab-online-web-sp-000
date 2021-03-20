03a9c8b75268897f68027533b677d2146727217b
Carrie Mathieu <carrieburks@gmail.com>
2021-03-20 13:22:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+// returns all drivers in driversArray, whose names match matchingString- case insensitive
+function findMatching(driversArray, matchingString) {
+    let matches = driversArray.filter(i => i.toLowerCase() == matchingString.toLowerCase())
+    return matches
+}
+
+// returns all drivers whose names begin w/ letters from string
+function fuzzyMatch (driversArray, matchingString) {
+    let matches = driversArray.filter(i => i[0] == matchingString[0])
+    return matches
+}
+
+// each element of driversArray is object that has name. return each element where name matches
+function matchName (driversArray, matchingString) {
+    let matches = driversArray.filter(i => i.name == matchingString)
+    return matches
+}

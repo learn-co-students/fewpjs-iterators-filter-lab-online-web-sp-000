1ccd7350dd867d5ea26823d9f64c69e482d110a4
Mike Barberry <mbarberry@Mikes-MacBook-Pro.local>
2021-03-26 18:19:41 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+findMatching = (list, driver) => {
+    return list.filter(function(name) { 
+        return name.toLowerCase() === driver.toLowerCase() 
+    });
+}
+
+function fuzzyMatch (list, letters) {
+    let lengthOfName = letters.length;
+    return list.filter(function(name) {
+        return name.slice(0, lengthOfName) === letters
+    })
+}
+
+function matchName(list, driver) {
+    return list.filter(function(name) {
+        return name.name.toLowerCase() === driver.toLowerCase()
+    })
+}

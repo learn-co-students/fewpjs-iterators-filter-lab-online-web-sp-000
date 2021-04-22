44a9f9a2c5f6fe0e4f8efad8183902a650badb53
Alison Williams <apw5011@gmail.com>
2021-04-22 17:10:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, string){
+    return drivers.filter(driverName => driverName.toUpperCase() === string.toUpperCase())
+}
+
+function fuzzyMatch(drivers, string) {
+    // returns a driver if beginning provided letters match
+    // does not return drivers if only middle or ending letters match
+    // does not return drivers if only middle or ending letters match
+    let stringLength = string.length;
+    return drivers.filter(driverName => driverName.slice(0, stringLength) === string)
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(driverObject => driverObject.name === string)
+}

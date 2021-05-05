1794ca314a4b45f722cd2d3e31611ad680ad5869
Evan Burton <evan@heyevan.com>
2021-05-04 18:18:02 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(drivers, string) {
+  return drivers.filter(el => el.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch (list, partialName) {
+    let lengthOfName = partialName.length;
+    return list.filter(function (driverName) {
+      return driverName.slice(0, lengthOfName) === partialName;
+    });
+  }
+  
+  function matchName (list, name) {
+    return list.filter(function (driver) {
+      return driver.name.toLowerCase() === name.toLowerCase();
+    });
+  }

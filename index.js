491551892ba62a6880414c5dd43b8ac300432b3a
dbrandt1990 <dvbrandt90@gmail.com>
2021-05-26 15:42:04 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(arr, str) {
+    return arr.filter(item => item.toLowerCase() == str.toLowerCase())
+}
+
+function fuzzyMatch(arr, str) {
+    return arr.filter(item => item.startsWith(str))
+}
+
+function matchName(arr, str) {
+    return arr.filter(item => item.name == str)
+}
